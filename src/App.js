94ae9dc5ac05e1daeed643b7e2cc32e016aba955
dead-drunk <posideloff@gmail.com>
2021-04-29 22:16:15 +0300

    add behaviors

--- src/App.js
+++ src/App.js
@@ -2,26 +2,31 @@ import React, { useState, useEffect } from 'react'
 import './App.css'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 
-const widgetsById = {
+const initialWidgetsById = {
   '1': {
     id: '1',
-    name: 'Widget 1'
+    name: 'Widget 1',
+    checked: false
   },
   '2': {
     id: '2',
-    name: 'Widget 2'
+    name: 'Widget 2',
+    checked: false
   },
   '3': {
     id: '3',
-    name: 'Widget 3'
+    name: 'Widget 3',
+    checked: false
   },
   '4': {
     id: '4',
-    name: 'Widget 4'
+    name: 'Widget 4',
+    checked: false
   },
   '5': {
     id: '5',
-    name: 'Widget 5'
+    name: 'Widget 5',
+    checked: false
   }
 }
 
@@ -31,10 +36,14 @@ const { history } = window
 
 const App = () => {
   const [ widgetsOrder, updateWidgetsOrder ] = useState(initialWidgetsOrder)
+  const [ widgetsById, updateWidgetsById ] = useState(initialWidgetsById)
+  const [ isEditable, setEditMode ] = useState(false)
+  const [ isAllSelected, setSelected ] = useState(false)
   const widgets = widgetsOrder.map(id => widgetsById[id])
 
   useEffect(() => {
-    const loadingWidgetsOrder = (history.state && history.state.prevWidgetsOrder) || initialWidgetsOrder
+    const loadingWidgetsOrder =
+      (history.state && history.state.prevWidgetsOrder) || initialWidgetsOrder
     updateWidgetsOrder(loadingWidgetsOrder)
   }, [])
 
@@ -47,25 +56,76 @@ const App = () => {
     history.pushState({ prevWidgetsOrder: items }, 'page')
   }
 
+  const checkHandler = id => {
+    updateWidgetsById(prev => ({
+      ...prev,
+      [id]: { ...prev[id], checked: !prev[id].checked }
+    }))
+  }
+
+  const selectHandler = () => {
+    updateWidgetsById(prev => {
+      const res = {}
+      console.log('prev1', prev)
+      for (let val in prev) {
+        res[val] = { ...prev[val], checked: true }
+      }
+      return res
+    })
+    setSelected(true)
+  }
+
+  const deselectHandler = () => {
+    updateWidgetsById(prev => {
+      const res = {}
+      for (let val in prev) {
+        res[val] = { ...prev[val], checked: false }
+      }
+      return res
+    })
+    setSelected(false)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Widgets</h1>
+        <div className="row">
+          <button
+            className={isAllSelected ? 'active' : ''}
+            onClick={selectHandler}>
+            Select All
+          </button>
+          <button
+            className={!isAllSelected ? 'active' : ''}
+            onClick={deselectHandler}>
+            Deselect All
+          </button>
+        </div>
         <DragDropContext onDragEnd={handleOnDragEnd}>
           <Droppable droppableId="widgets">
             {provided => (
               <ul
-                className="widgets"
+                className={isEditable ? 'widgets editable' : 'widgets'}
                 {...provided.droppableProps}
                 ref={provided.innerRef}>
-                {widgets.map(({ id, name }, index) => {
+                {widgets.map(({ id, name, checked }, index) => {
                   return (
-                    <Draggable key={id} draggableId={id} index={index}>
+                    <Draggable
+                      key={id}
+                      isDragDisabled={!checked || !isEditable}
+                      draggableId={id}
+                      index={index}>
                       {provided => (
                         <li
                           ref={provided.innerRef}
                           {...provided.draggableProps}
                           {...provided.dragHandleProps}>
+                          <span
+                            className={
+                              checked ? 'checkbox checked' : 'checkbox'
+                            }
+                            onClick={() => checkHandler(id)}
+                          />
                           <p>{name}</p>
                         </li>
                       )}
@@ -78,6 +138,9 @@ const App = () => {
           </Droppable>
         </DragDropContext>
       </header>
+      <button className="editBtn" onClick={() => setEditMode(prev => !prev)}>
+        {!isEditable ? 'Edit widgets' : 'Done'}
+      </button>
     </div>
   )
 }
